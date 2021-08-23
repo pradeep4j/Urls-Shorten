acd93d3fd31d4ada35bf82f1aac10d887de886ff
Pradeep <pradeepmaurya@gmail.com>
2021-08-23 13:46:49 +0530

    some changes

---
+++ login.php
@@ -0,0 +1,156 @@
+<?php 
+  include "config.php";
+  $new_url = "";
+  if(isset($_GET)){
+    foreach($_GET as $key=>$val){
+      $u = mysqli_real_escape_string($conn, $key);
+      $new_url = str_replace('/', '', $u);
+    }
+      $sql = mysqli_query($conn, "SELECT full_url FROM url WHERE shorten_url = '{$new_url}'");
+      if(mysqli_num_rows($sql) > 0){
+		  if(isset($_GET['id'])){		  
+				$sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 and user_id=".$_GET['id']." WHERE shorten_url = '{$new_url}'");
+				if($sql2){
+					$full_url = mysqli_fetch_assoc($sql);
+					header("Location:".$full_url['full_url']);
+				  }
+		  }
+		  else{
+			  $sql2 = mysqli_query($conn, "UPDATE url SET clicks = clicks + 1 WHERE shorten_url = '{$new_url}'");
+				if($sql2){
+					$full_url = mysqli_fetch_assoc($sql);
+					header("Location:".$full_url['full_url']);
+				}
+      
+		  }
+	  }
+	  }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>URL Shortener</title>
+  <link rel="stylesheet" href="style.css">
+  <!-- Iconsout Link for Icons -->
+  <link rel="stylesheet" href="https://unicons.iconscout.com/release/v3.0.6/css/line.css">
+  <style>
+    @import url('https://fonts.googleapis.com/css?family=Poppins:400,500,600,700&display=swap');
+    nav{
+        padding-left: 100px!important;
+        padding-right: 100px!important;
+        background: #20B2AA;
+        font-family: 'Poppins', sans-serif;
+    } 
+    nav a.navbar-brand{
+        color: #fff;
+        font-size: 30px!important;
+        font-weight: 500;
+    }
+    button a{
+        color: #20B2AA;
+        font-weight: 500;
+    }
+    button a:hover{
+        text-decoration: none;
+    }
+    h1{
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        width: 100%;
+        text-align: center;
+        transform: translate(-50%, -50%);
+        font-size: 50px;
+        font-weight: 600;
+    }
+    </style>
+</head>	
+<body>
+  <div class="wrapper"><button type="button" ><a href="logout-user.php">Logout</a></button>
+    <form action="#" autocomplete="off">
+      <input type="text" spellcheck="false" name="full_url" placeholder="Enter or paste a long url" required>
+      <i class="url-icon uil uil-link"></i>
+      <button>Shorten</button>
+	  <input type="hidden" name="id" value='<?php echo $_REQUEST['id'];?>'>
+    </form>
+    <?php
+      $sql2 = mysqli_query($conn, "SELECT * FROM url where users_id='".$_SESSION['id']."' ORDER BY id DESC");
+      if(mysqli_num_rows($sql2) > 0){;
+        ?>
+          <div class="statistics">
+            <?php
+              $sql3 = mysqli_query($conn, "SELECT COUNT(*) FROM url");
+              $res = mysqli_fetch_assoc($sql3);
+
+              $sql4 = mysqli_query($conn, "SELECT clicks FROM url");
+              $total = 0;
+              while($count = mysqli_fetch_assoc($sql4)){
+                $total = $count['clicks'] + $total;
+              }
+            ?>
+            <span>Total Links: <span><?php echo end($res) ?></span> & Total Clicks: <span><?php echo $total ?></span></span>
+            <a href="delete.php?delete=all">Clear All</a>
+        </div>
+        <div class="urls-area">
+          <div class="title">
+            <li>Shorten URL</li>
+            <li>Original URL</li>
+            <li>Clicks</li>
+            <li>Action</li>
+          </div>
+          <?php
+            while($row = mysqli_fetch_assoc($sql2)){
+              ?>
+                <div class="data">
+                <li>
+                  <a href="<?php echo $row['shorten_url'] ?>" target="_blank">
+                  <?php
+                    if($domain.strlen($row['shorten_url']) > 50){
+                      echo $domain.substr($row['shorten_url'], 0, 50) . '...';
+                    }else{
+                      echo $domain.$row['shorten_url'];
+                    }
+                  ?>
+                  </a>
+                </li> 
+                <li>
+                  <?php
+                    if(strlen($row['full_url']) > 60){
+                      echo substr($row['full_url'], 0, 60) . '...';
+                    }else{
+                      echo $row['full_url'];
+                    }
+                  ?>
+                </li> 
+              </li>
+                <li><?php echo $row['clicks'] ?></li>
+                <li><a href="delete.php?id=<?php echo $row['shorten_url'] ?>">Delete</a></li>
+              </div>
+              <?php
+            }
+          ?>
+      </div>
+        <?php
+      }
+    ?>
+  </div>
+
+  <div class="blur-effect"></div>
+  <div class="popup-box">
+  <div class="info-box">Your short link is ready. You can also edit your short link now but can't edit once you saved it.</div>
+  <form action="#" autocomplete="off">
+    <label>Edit your shorten url</label>
+    <input type="text" class="shorten-url" spellcheck="false" required>
+    <i class="copy-icon uil uil-copy-alt"></i>
+	<input type="hidden" name="ids" value='<?php echo $_REQUEST['id'];?>'>
+    <button>Save</button>
+  </form>
+  </div>
+
+  <script src="script.js"></script>
+
+</body>
+</html>
+
---
+++ url.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2021 at 05:25 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userdemo`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url`
+--
+
+CREATE TABLE `url` (
+  `id` int(11) NOT NULL,
+  `users_id` int(11) NOT NULL,
+  `shorten_url` varchar(200) NOT NULL,
+  `full_url` varchar(1000) NOT NULL,
+  `clicks` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `url`
+--
+
+INSERT INTO `url` (`id`, `users_id`, `shorten_url`, `full_url`, `clicks`) VALUES
+(53, 0, '1f4af', 'http://facebook.com/pradeep4july', 1),
+(54, 0, '0615e', 'http://google.com', 1),
+(55, 0, '5d328', 'http://yahoo.com', 1),
+(65, 0, '365a4', 'http://neave.com', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `url`
+--
+ALTER TABLE `url`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `url`
+--
+ALTER TABLE `url`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 14, 2021 at 05:22 PM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 7.4.21
+-- phpMyAdmin SQL Dump
+-- version 5.1.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 26, 2021 at 03:11 PM
+-- Server version: 10.4.18-MariaDB
+-- PHP Version: 8.0.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `userform`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `usertable`
+--
+
+CREATE TABLE `usertable` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `code` mediumint(50) NOT NULL,
+  `status` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `usertable`
+--
+ALTER TABLE `usertable`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `usertable`
+--
+ALTER TABLE `usertable`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
